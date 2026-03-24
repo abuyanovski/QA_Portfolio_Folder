051d0c94370753129c64828c25a876eea1b5acc6
abuyanovski <39982554+abuyanovski@users.noreply.github.com>
2026-03-24 12:11:27 -0700

    Refactor page classes and methods for improved structure and readability

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,36 +1,33 @@
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+class LoginPage:
+    #                                       ==PAGE URLs==
+    INVENTORY_PAGE_URL = "https://www.saucedemo.com/inventory.html"
 
-# == PAGE ACTIONS ==
-class BasePage:
+    #                                       ==PAGE OBJECTS==
+    INVENTORY_CONTAINER = (By.XPATH, "//div[@id='inventory_container']//div//div[@id='inventory_container']")
+    INVENTORY_PAGE_TITLE = (By.CSS_SELECTOR, ".title")
+    CART_BUTTON = (By.CSS_SELECTOR, ".shopping_cart_link")
+    BURGER_MENU_BUTTON = (By.CSS_SELECTOR, ".shopping_cart_link")
+
+    # is_loaded()
+    # get_inventory_items_count()
+
+
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
 
-    def open_url(self, url):
-        self.driver.get(url)
 
-    def wait_for_visibility(self, locator):
-        return self.wait.until(
-            EC.visibility_of_element_located(locator)
-        )
-
-
-
-    #
-    # click()
-    # type_text()
-    # wait_for_visibility()
-    # wait_for_clickable()
-    # is_element_present()
-    # get_current_url()
-
-
-
-
-
-
+        # open_the_page()
+        # enter_username()
+        # enter_password()
+        # click_login()
+        # login()
+        # is_inventory_page_loaded()
+        # is_error_message_displayed()
 
         class BasePage:
             def __init__(self, driver: WebDriver):
